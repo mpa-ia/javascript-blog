548c65effd4ed295f709f83aa1f621469836b257
Marysia Pabich <marysia.pabich@gmail.com>
2019-09-11 20:50:19 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -8,19 +8,22 @@ document.getElementById('test-button').addEventListener('click', function() {
 */
 
 const titleclickHandler = function () {
+    const clickedElement = this;
     console.log('link was clicked!');
-    console.log(event);
 
-/* remove class 'active' from all article links */
+/* [DONE] remove class 'active' from all article links */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
 for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
 }
 
-/* add class 'active' to the clicked link */
+/* [IN PROGRESS] add class 'active' to the clicked link */
 
-/* remove class 'active' from all articles */
+clickedElement.classList.add('active');
+console.log('clickedElement: ', clickedElement);
+
+/* [DONE] remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('.posts .post.active');
 for (let activeArticle of activeArticles) {
