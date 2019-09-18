bafcd4b4e85a8b56d83c64f381c877e6954d7a1f
Marysia Pabich <marysia.pabich@gmail.com>
2019-09-18 15:42:17 +0200

    Get tags from data-tags attributes for generateTags function

--- js/script.js
+++ js/script.js
@@ -115,6 +115,8 @@
       /* make html variable with empty string */
       let html = '';
       /* get tags from data-tags attribute */
+      const articleTags = article.getAttribute('data-tags');
+      console.log(articleTags);
       /* split tags into array */
       /* START LOOP: for each tag */
         /* generate HTML of the link */
