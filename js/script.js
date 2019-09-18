b16c1de31796bd71e24f217367b1369d7f1a1328
Marysia Pabich <marysia.pabich@gmail.com>
2019-09-18 15:57:56 +0200

    Generate HTML of the link inside loop for tags

--- js/script.js
+++ js/script.js
@@ -124,6 +124,8 @@
       for (let tag of articleTagsArray) {
         console.log(tag);
         /* generate HTML of the link */
+        const linkHTML = '<li><a href="#tag-' + tag + '">' + tag  + '</a></li>';
+        console.log(linkHTML);
         /* add generated code to html variable */
       /* END LOOP: for each tag */
       }
